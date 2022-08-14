16346091b84414b4d8ef5123c747f23e8cb3e240
marina <litvinova_06@bk.ru>
2022-08-14 16:46:32 +0500

    homework

---
+++ Work_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// // Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+int [] array = {4,5,6};
+int n = array.Length;
+int find = 5;
+int index = 0;
+while (index < n)
+{
+    {if (array [index]==find);
+    }
+    index++;
+}
+Console.WriteLine(array[1]);
+
+int [] array = {7,8,2};
+int n = array.Length;
+int find = 8;
+int index = 0;
+while (index < n)
+{
+    {if (array [index]==find);
+    }
+    index++;
+}
+Console.WriteLine(array[1]);
+
+int [] array = {9,1,8};
+int n = array.Length;
+int find = 1;
+int index = 0;
+while (index < n)
+{
+    {if (array [index]==find);
+    }
+    index++;
+}
+Console.WriteLine(array[1]);
